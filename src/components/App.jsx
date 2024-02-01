55565763fdc178732ca34f4b03dbd387e25eca66
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-02-01 13:23:19 +0200

    in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { getItemsApi } from './Api/Api';
+
+export class App extends Component {
+  state = {
+    inputData: '',
+    images: [],
+    page: 1,
+  };
+
+  inputData = e => {
+    e.preventDefault();
+    // const newSearch = e.target.search.value.toLowerCase().trim();
+    this.setState({ inputData: e.target.search.value.toLowerCase().trim() });
+    // console.log('newSearch', newSearch);
+    // console.log('state ', this.state);
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.inputData !== this.state.inputData ||
+      prevState.page !== this.state.page
+    ) {
+      try {
+        const responseApi = await getItemsApi(
+          this.state.inputData,
+          this.state.page
+        );
+        console.log('responseApi', responseApi);
+        this.setState({ images: responseApi });
+      } catch (error) {}
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.inputData} />
+        <ImageGallery images={this.images} />
+      </div>
+    );
+  }
+}
